6ee4dfbe8f549a53a660d052d7fe8ec19509c7fb
mdrefathossen <refathossen2001@gmail.com>
2023-11-18 14:09:31 +0600

    basic setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8080
+app.get('/',(req,res) => {
+    res.send('Hellow World')
+})
+app.listen(port,() =>{
+    console.log(`Example app listening on port ${port}`)
+})
