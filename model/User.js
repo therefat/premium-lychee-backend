fd3b0a45c5f0da27bdae80a0215972b9d1f484f0
mdrefathossen <refathossen2001@gmail.com>
2023-11-20 14:30:09 +0600

    user model create done

--- model/User.js
+++ model/User.js
@@ -1,1 +1,100 @@
-const mon
+const mongoose = require('mongoose');
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  email : {
+    type : String,
+    required : true,
+    trim : true,
+    lowercase : true,
+    unique : true,
+    validator(value){
+        if(!validator.isEmail(value)){
+            throw new Error("Email is invalid")
+        }
+    }
+  },
+  password: {
+    type : String,
+    required : true,
+    minLength : 7,
+    trim : true,
+    validate(value){
+        if(value.toLowerCase().includes('password')){
+            throw new Error('password must contain password')
+        }
+    }
+  },
+  tokens : [{
+    token : {
+        type : String,
+        required : true
+    }
+  }]
+},{
+  timestamps: true 
+});
+
+//Generate auth token
+userSchema.methods.generateAuthToken = async function () {
+  const user = this 
+  const token = jwt.sign({_id: user._id.toString()},
+  process.env.JWT_SECRET,{
+      expiresIn: '2d'
+  }) 
+  user.tokens = user.tokens.concat({token})
+  await user.save()
+  return token
+}
+
+//login in users
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })  // U line
+  if (!user) {
+      throw new Error('Unable to log in')
+  }
+  const isMatch = await bcrypt.compare(password, user.password)
+  console.log(isMatch)
+  if(!isMatch) {
+      throw new Error('Unable to login')
+  }
+
+  return user
+}
+//Hash plain password before saving
+userSchema.pre('save',async function(next){
+  const user = this 
+  if(user.isModified('password')){
+      user.password = await bcrypt.hash(user.password,8)
+  }
+  next()
+})
+const User = mongoose.model('User', userSchema)
+module.exports = User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
